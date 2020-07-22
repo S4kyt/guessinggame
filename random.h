07495bc065ee1350e4bd3965db66c0a17dd14afc
Slapjacky <jerikolo1@gmail.com>
2020-07-22 16:53:44 +0200

    Added random.cpp + random.h

---
+++ random.h
@@ -0,0 +1,5 @@
+#pragma once //not exactly sure what this does, but as i've read its an include guard, kinda like a restriction to make you fail less later on from what i understand
+
+//int winningAnswer; //im not sure what's a good way to transition data between files so i did this.
+
+int generateAnswer();
